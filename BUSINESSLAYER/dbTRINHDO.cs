fbc0363b5c89a3c876ff2b52b50e11cd6217fba3
natwodev <nhuynhnamht2023@gmail.com>
2024-12-13 09:44:24 -0800

    Mô tả thay đổi bạn đã thực hiện

---
+++ BUSINESSLAYER/dbTRINHDO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DATALAYER;
+
+namespace BUSINESSLAYER
+{
+    public class dbTRINHDO
+    {
+        QLNHANSUEntities db = new QLNHANSUEntities();
+        public List<TRINHDO> getList()
+        {
+            return db.TRINHDOes.ToList();
+        }
+    }
+}
--- QLNHANSU/Program.cs
+++ QLNHANSU/Program.cs
@@ -17,7 +17,7 @@ namespace QLNHANSU
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmTest());
         }
     }
 }
